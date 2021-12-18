3c415d618beca123f1216856531e94cf9f941b0f
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-12-18 19:28:20 +0200

    Исправил api

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -47,15 +47,14 @@ const App = () => {
     setLargeImg(data);
   };
 
-  const fetchPick = () => {
-    return fetch(
+  const fetchPick = async () => {
+    const res = await fetch(
       `${URL}?q=${queryName}&key=${API_KEY}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`
-    ).then((res) => {
-      if (res.status === 404) {
-        return Promise.reject("Oops, something went wrong");
-      }
-      return res.json();
-    });
+    );
+    if (res.status === 404) {
+      return Promise.reject("Oops, something went wrong");
+    }
+    return await res.json();
   };
 
   const toggleModal = () => {
